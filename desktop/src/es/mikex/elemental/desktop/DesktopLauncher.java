07e481e27dbb05f8a357fa74b020f86f00d768df
Miguel <miguelaguero98@gmail.com>
2016-10-04 21:01:39 +0200

    Update all files!

--- desktop/src/es/mikex/elemental/desktop/DesktopLauncher.java
+++ desktop/src/es/mikex/elemental/desktop/DesktopLauncher.java
@@ -3,13 +3,10 @@ package es.mikex.elemental.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import es.mikex.elemental.Main;
-import es.mikex.elemental.settings.Settings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Main(), config);
-		config.width = 1280;
-		config.height = 720;
 	}
 }
